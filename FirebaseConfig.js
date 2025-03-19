09adc6be8ee1f6d616c94b74b83abf15a71d1e18
ch3AtZ <91371856+ch3AtZ@users.noreply.github.com>
2025-03-19 15:01:44 +0530

    Delete FirebaseConfig.js

--- FirebaseConfig.js
+++
@@ -1,7 +0,0 @@
-import { initializeAuth } from "@react-native-firebase/auth";
-import { getAuth } from "@react-native-firebase/auth";
-
-const firebaseConfig = {
-    apiKey : "your fjucking rjgn wjgnfweo"
-
-}
