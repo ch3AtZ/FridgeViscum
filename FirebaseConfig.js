02b6053db3209132cf0583032c4847f78f3f6389
ch3AtZ <dhruvbharara47@gmail.com>
2025-03-19 12:54:32 +0530

    firebase authentication

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -2,5 +2,6 @@ import { initializeAuth } from "@react-native-firebase/auth";
 import { getAuth } from "@react-native-firebase/auth";
 
 const firebaseConfig = {
-    
+    apiKey : "your fjucking rjgn wjgnfweo"
+
 }
