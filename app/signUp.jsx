50ac4c2df31b7408a191884444b296568f711802
ch3AtZ <dhruvbharara47@gmail.com>
2025-04-21 12:54:15 +0530

    fixing errors

--- app/signUp.jsx
+++ app/signUp.jsx
@@ -57,7 +57,7 @@ const styles = StyleSheet.create({
     },
     input:{
         width:"90%",
-        backgroundColor: "rgba(255, 255, 255, 0.2)",
+        backgroundColor: "rgba(255, 255, 255, 0.5)",
         padding: 12,
         borderRadius: 10,
         color: "#FFF",
